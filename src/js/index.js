39ffeffed1b5a3fd07bb07d82d8d235bc578c166
Alessio Pisanu <alessio.pisanu@accenture.com>
2020-02-12 18:05:07 +0100

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,14 @@
 const btn = document.querySelectorAll('.btn');
 const checkBox = document.querySelector('#checkbox');
 
-btn.forEach(item => {
-    console.log(btn)
-    item.addEventListener('click', event => {
-        item.classList.toggle('btn-green');
+if (btn.length > 0 || checkBox) {
+    btn.forEach(item => {
+        console.log(btn)
+        item.addEventListener('click', event => {
+            item.classList.toggle('btn-green');
+        });
+    })
+    checkBox.addEventListener('change', event => {
+        console.log(event)
     });
-})
-
-
-checkBox.addEventListener('change', event => {
-    console.log(event)
-});
+}
